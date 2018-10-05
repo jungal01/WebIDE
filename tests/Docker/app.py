564f63334ed93ca277292dacda7dd105e688df62
kevincobble@gmail.com <kevincobble@gmail.com>
2018-10-05 13:52:34 -0500

    Basic Flask Server

--- tests/Docker/app.py
+++ tests/Docker/app.py
@@ -14,12 +14,12 @@ def read_output(file):
 @app.route('/')
 def hello_world():
     # Starts a docker and runs the hello world that comes with docker. Runs the docker container with the id/name of Hello, so it can be stopped
-    os.system("docker run -name='Hello' hello-world > hello.txt")
+    os.system("docker run --name='Hello' hello-world > hello.txt")
     hello = read_output('hello.txt')
     # currently non functioning, but should stop the container named hello However, it does not return an output if this is done, though it does appear to stop
-    os.system('docker stop hello')
+    os.system('docker stop Hello')
     # this is the command to remove a docker container
-    os.system('docker rm hello')
+    os.system('docker rm Hello')
     # I was using the info.txt to actually see if the docker was running or not
     os.system('docker info > info.txt')
     info = read_output('info.txt')
