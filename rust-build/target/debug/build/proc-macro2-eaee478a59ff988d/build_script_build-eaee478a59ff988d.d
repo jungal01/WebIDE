85b48198ed81704d53f2ef7bbed975d57b209f85
Allen <jungal01@luther.edu>
2018-11-09 17:50:59 -0600

    make README and rename project build

---
+++ rust-build/target/debug/build/proc-macro2-eaee478a59ff988d/build_script_build-eaee478a59ff988d.d
@@ -0,0 +1,5 @@
+/home/allen/Dropbox/cs490/rust-build/target/debug/build/proc-macro2-eaee478a59ff988d/build_script_build-eaee478a59ff988d: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.21/build.rs
+
+/home/allen/Dropbox/cs490/rust-build/target/debug/build/proc-macro2-eaee478a59ff988d/build_script_build-eaee478a59ff988d.d: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.21/build.rs
+
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-0.4.21/build.rs:
