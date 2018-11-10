46f9a531fbaac1d28a2689bd82ce9ca45f3472c2
Allen <jungal01@luther.edu>
2018-11-09 19:01:31 -0600

    merge frontend

---
+++ rust-build/target/debug/deps/log-2e47b3edfb87e81c.d
@@ -0,0 +1,7 @@
+/home/allen/Dropbox/cs490/rust-build/target/debug/deps/liblog-2e47b3edfb87e81c.rlib: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/lib.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/macros.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/serde.rs
+
+/home/allen/Dropbox/cs490/rust-build/target/debug/deps/log-2e47b3edfb87e81c.d: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/lib.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/macros.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/serde.rs
+
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/lib.rs:
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/macros.rs:
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.6/src/serde.rs:
---
+++ rust-build/target/debug/deps/quote-cd2f5c99e19cad85.d
@@ -0,0 +1,7 @@
+/home/allen/Dropbox/cs490/rust-build/target/debug/deps/libquote-cd2f5c99e19cad85.rlib: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/lib.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/ext.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/to_tokens.rs
+
+/home/allen/Dropbox/cs490/rust-build/target/debug/deps/quote-cd2f5c99e19cad85.d: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/lib.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/ext.rs /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/to_tokens.rs
+
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/lib.rs:
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/ext.rs:
+/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-0.6.10/src/to_tokens.rs:
