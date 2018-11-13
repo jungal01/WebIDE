396759be2a77db9e597ab0cff9eef34c8d4fe8ac
Allen <jungal01@luther.edu>
2018-11-13 15:48:09 -0600

    remove target dir and update gitignore

--- rust-build/target/debug/deps/wasm_bindgen_shared-7d88242f58625503.d
+++
@@ -1,5 +0,0 @@
-/home/allen/Dropbox/cs490/rust-build/target/debug/deps/libwasm_bindgen_shared-7d88242f58625503.rlib: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.27/src/lib.rs
-
-/home/allen/Dropbox/cs490/rust-build/target/debug/deps/wasm_bindgen_shared-7d88242f58625503.d: /home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.27/src/lib.rs
-
-/home/allen/.cargo/registry/src/github.com-1ecc6299db9ec823/wasm-bindgen-shared-0.2.27/src/lib.rs:
