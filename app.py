bc6c477d748fa3f78600329c5de7c56bdb8e9da5
Allen <jungal01@luther.edu>
2019-04-16 15:45:50 -0500

    merge with kevin

--- app.py
+++ app.py
@@ -1,91 +1,157 @@
-from flask import Flask, request, render_template, make_response
-import json
-import os
-import identify_language as language
+import sys, os, random, json
+#import identify_langauge as language
+from flask import *
+from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
 
+UPLOAD_FOLDER="/uploads"
+ALLOWED_EXTENSIONS = set(['txt','py','js','java','cpp','c','rs'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/getfile', methods=['GET','POST'])
+def getfile():
+    if request.method == 'POST':
+        #file = request.form['param1']
+        file= request.files['myfile']
+        filename = secure_filename(file.filename)
+        
+        file.save(os.path.join("importCode",filename))
+    
+        if allowed_file(filename):
+            with open("./importCode/"+filename) as f:
+                file_content = f.read()
+                f.close()
+            fileInfo = filename.rsplit('.',1)[0]+"=."+filename.rsplit('.',1)[1].lower()+"::"+file_content
+            print(filename)
+            print(file_content)
+            print(fileInfo)
+            return render_template('index.html',fileContent=file_content,uploadedName=filename,data = fileInfo)
+    
+    return render_template('index.html',data="Error=AI:Failure to load File")
+
+def allowed_file(filename):
+    extension = filename.rsplit('.',1)[1].lower()
+    if ('.' in filename and extension in ALLOWED_EXTENSIONS):
+        return True
+    return False
+        
 @app.route('/')
-def index():
+def index(): #Serves up index template
+    print("Rendering index html")
     return render_template('index.html')
 
 @app.route('/about')
-def aboutPage():
-    return render_template('index_about.html')
+def aboutPage(): #Serves up about template
+    print("Rendering about html")
+    return render_template('about.html')
 
 @app.route('/compile', methods=['POST'])
-def compile():
-    print('Getting request')
-    langError = None
+def compile(): #Takes code from JS and returns output to JS
     os.system("echo ''>errFile.txt")
-    try:
-        os.system('rm view.txt') #del for windows, rm for linux
-    except:
-        pass
-    #name = 'view.txt' # used in testing
-    name = request.form['codeboxname'] #used in
-    print(name)
-    txt = request.form['codeBox01']
-    try:
-        #AI NOT WORK
-        #if name == 'view.txt':
-            ##Call AI
-            #version = 'Unknown'
-            #with open(name, 'w') as f:
-                #f.write(txt)
-                #f.close()
-            #version = str(language.identify(name))
-            ## if statement to create appropriately named file
-            #if version == 'java':
-                #name = 'view.java'
-            #elif version == 'c':
-                #name = 'view.c'
-            #elif version == 'cpp':
-                #name = 'view.cpp'
-            #elif version == 'rust':
-                #name = 'view.rs'
-            #else:
-                #langError = "Language not supported"
-        print('Checking version')
-        # Checks what version it is by the name that was entered
-        if name[-2:] == '.c':
-            version = 'c'
-        elif name[-4:] == '.cpp':
-            version = 'cpp'
-        elif name[-3:] == '.py':
-            version = 'python3'
-        elif name[-3:] == '.rs':
-            version = 'rust'
-        else:
-            langError = "Language not supported"
-        print('Got Version')
-        #will put this in a while loop if we have multiple names over all from the AI Currently its just for one file
-        print("opening and writing to the file to create")
-        with open(name,'w') as f:
-            f.write(txt)
-            f.close()
-
-        print("calling compiler")
-        os.system('./call-compiler {} {}'.format(version,name)) #bash is used for windows ./ is for linux
-        print("Finished compiling")
-
-        errorFile = open('errFile.txt','r')
-
-        error = errorFile.read()
-        errorFile.close()
-
-        if error != '': # returns error cases to output if error occured
-            return render_template("index.html",output='Your code is: '+version+'\n' + error, inputbox=txt,filename=name)
-        elif langError != None:
-            return render_template("index.html",output='Your code is: '+version+'\n' + langError, inputbox=txt,filename=name)
-        else: # The webassembly should take care of producing the code in the html without having to be passed here
-            pass
-            #return render_template("index.html",inputbox=txt, filename=name)
-    except UnboundLocalError as e:
+    os.system("echo ''>output.txt")
+    version = None
+    err = ''
+    print("Got code run request")
+    if request.method == "POST":
+        code = request.form['param1']
+        lang = request.form['param2']
+        name = request.form['param3']
+    print("Got code, language or api, name")
+    print("Code: "+code)
+    print("Lang or AI: "+lang)
+    print("Name: "+name)
+    
+    try: # Try to call the specific language
+        if lang != 'AI':
+            if lang == '.js':
+                version = 'js'
+            elif lang == '.java':
+                version = 'java'
+            elif lang == '.c':
+                version = 'c'
+            elif lang == '.cpp':
+                version = 'cpp'
+            elif lang == '.py':
+                version = 'python'
+            elif lang == '.rs':
+                version = 'rust'
+            else:
+                raise Exception("Language not supported")
+    except Exception as e:
+        os.system("echo {} >> errFile.txt".format("LANGUAGE ERROR: "))
         print(e)
-        return render_template("index.html",inputbox=txt,filename=name,output=e)
-    #auto populates if there is no error
+        os.system("echo {} >> errFile.txt".format(e))
+        os.system("echo {} >> errFile.txt".format('\n'))    
+
+    try: # Try to call the API 
+        if lang=='AI':
+            #if lang != None:
+            raise Exception("Language entered and API detection requested")
+            ##Call AI
+            #lang = '.txt'
+            #version = language.identify(filename+lang)
+            #print("Identified language as: "+version)
+            
+    except Exception as e:
+        os.system("echo {} >> errFile.txt".format("API ERROR: "))
+        print(e)
+        os.system("echo {} >> errFile.txt".format(e))
+        os.system("echo {} >> errFile.txt".format('\n'))
+        
+    try: # get language from version, from API
+        if lang == '.txt':
+            if version == 'java':
+                lang = '.java'
+            elif version == 'js':
+                lang = '.js'
+            elif version == 'c':
+                lang = '.c'
+            elif version == 'cpp':
+                lang = '.cpp'
+            elif version == 'python':
+                lang='.py'
+            elif version == 'rust':
+                lang = '.rs'
+            else:
+                raise Exception("Language is not supported")
+    except Exception as e:
+        os.system("echo {} >> errFile.txt".format("VERSION ERROR: "))
+        print(e)
+        os.system("echo {} >> errFile.txt".format(e))
+        os.system("echo {} >> errFile.txt".format('\n'))  
+    
+        
+    try: # Try to write the code file
+        with open(name+lang, 'w') as f:
+            f.write(code)
+            f.close
+    except Exception as e:
+        os.system("echo {} >> errFile.txt".format("WRITING ERROR: "))
+        print(e)
+        os.system("echo {} >> errFile.txt".format(e))
+        os.system("echo {} >> errFile.txt".format('\n'))	
+    
+    try: # Create & send response to send to javascript
+        os.system("./call-compiler {} {}".format(version, name+lang))
+        with open('output.txt', 'r') as f:
+            result = f.read()
+            f.close()
+            
+        print('results: '+result)
+        if result == '' or result == '\n':
+            raise Exception("COMPILE ERROR: Empty file output.txt")
+    except Exception as e:
+        os.system("echo {} >> errFile.txt".format("RESPONSE ERROR: "))
+        print(e)
+        os.system("echo {} >> errFile.txt".format(e))
+        os.system("echo {} >> errFile.txt".format('\n'))
+        result = str(e)
+    os.system("rm {}".format(name+lang))
+    return make_response(result)
+
+
+
 
 if __name__== '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, port=5000, threaded=True, host=('0.0.0.0')) #, host='127.0.0.1', port=5000    
