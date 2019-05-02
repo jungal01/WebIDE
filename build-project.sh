cb562db05bb058f6d3408ecca39c6f484541a140
root <root@ws7733.lc.luther.edu>
2019-05-02 14:50:03 -0500

    added build-project

--- build-project.sh
+++ build-project.sh
@@ -8,51 +8,74 @@ echo
 
 if [[ "$yni" = "y" ]]; then
   # install the CUDA version for ubuntu for the AI
-  echo "Adding repositories"
+  echo "installing CUDA"
   echo
   sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
   sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
-  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-  sudo add-apt-repository ppa:openjdk-r/ppa -y
-  sudo add-apt-repository ppa:deadsnakes/ppa -y
-  sudo add-apt-repository ppa:pypy/ppa -y
   sudo apt update
+  sudo apt upgrade -y
+  sudo apt-get install cuda
+  # end cuda
+
 
   echo
-  echo "installing compilers"
+  echo "updating Gnu Compiler Collection to 8.1"
   echo
-  sudo apt-get install cuda
+  # update gcc to latest version
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  sudo apt update && sudo apt upgrade -y
   sudo apt install gcc-8 g++-8 -y
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
-  sudo apt install openjdk-11-jdk -y
-  curl -sf -L https://static.rust-lang.org/rustup.sh | sh
-  echo 'export PATH=.:~/.cargo/bin:$PATH' >> ~/.bashrc
-
-  sudo apt install build-essential xz-utils curl
-  curl -SL http://releases.llvm.org/7.0.1/clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJC .
-  mv clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-18.04 clang_7.0.1
-  sudo mv clang_7.0.1 /usr/local/
-  echo 'export PATH=/usr/local/clang_7.0.1/bin:$PATH' >> ~/.bashrc
-  echo 'export LD_LIBRARY_PATH=/usr/local/clang_7.0.1/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-
-  sudo apt install tcc #tcc
-  source ~/.bashrc
-  sudo apt install pypy -y
-  sudo apt install pypy3 -y
+  # end gcc
 
 
   echo
   echo "installing programming languages"
   echo
   # language installations
+  # gcc options
   sudo apt install gnat-4.9 -y # ada
   sudo apt install gfortran-8 -y # fortran
   sudo apt install gccgo -y # go
+
+  #other languages
+  # OpenJDK Java
+  sudo add-apt-repository ppa:openjdk-r/ppa -y
+  sudo apt update && sudo apt upgrade -y
+  sudo apt install openjdk-11-jdk -y
+  # end Java
+  # Rust
+  curl -sf -L https://static.rust-lang.org/rustup.sh | sh
+  echo 'export PATH=.:~/.cargo/bin:$PATH' >> ~/.bashrc
+  source ~/.bashrc
+  # end Rust
   sudo apt install lua5.3 -y # Lua
   sudo apt install ruby-full -y # ruby
+  # python latest stable version
+  sudo add-apt-repository ppa:deadsnakes/ppa -y
   sudo apt install python3.7 -y
   sudo apt install python3.6 -y
-  sudo apt install nodejs -y
+  # end python
+
+
+  echo
+  echo "installing alternative compilers"
+  echo
+  # Compilers
+  # Clang
+  sudo apt install build-essential xz-utils curl
+  curl -SL http://releases.llvm.org/7.0.1/clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJC .
+  mv clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-18.04 clang_7.0.1
+  sudo mv clang_7.0.1 /usr/local/
+  echo 'export PATH=/usr/local/clang_7.0.1/bin:$PATH' >> ~/.bashrc
+  echo 'export LD_LIBRARY_PATH=/usr/local/clang_7.0.1/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+  # end clang
+  # Pypy
+  sudo add-apt-repository ppa:pypy/ppa -y
+  sudo apt install pypy -y
+  sudo apt install pypy3 -y
+  # end pypy
+  sudo apt install tcc #tcc
 
 
   echo
