4e71962809748fdd0a8a9b7b06bf7f3009fed3ac
Allen <jungal01@luther.edu>
2019-05-02 16:08:02 -0500

    clean branch for merge with master

--- ai/runrepickle.py
+++
@@ -1,4 +0,0 @@
-from deeplearning.fileinput import repickle, pkl_dir
-
-if __name__ == '__main__':
-    repickle(pkl_dir, n_proc=8)
--- ai/runtrain.py
+++
@@ -1,4 +0,0 @@
-from deeplearning.train import mixed_bow_transformer_cache
-
-if __name__ == '__main__':
-    mixed_bow_transformer_cache(load=False)
